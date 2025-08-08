ca6491d33c328080edc073d0f71a1a3138b8b5f2
Victor Vida <168488457+victorvida12@users.noreply.github.com>
2025-08-08 23:18:50 +0000

    Métodos Getters e Setters

--- Agosto8/Main.java
+++ Agosto8/Main.java
@@ -4,6 +4,12 @@ public class Main{
 
     public static void main(String[] args){
         Pessoa obj1 = new Pessoa();
-        Pessoa obj2 = new Pessoa("Joaquin," 1);
+        Pessoa obj2 = new Pessoa("Joaquin", 1);
+        Pessoa obj3 = new Pessoa("Victor", 19, 12345678912l);
+        String nome = obj3.getNome();
+
+        System.out.println(obj1.toString());
+        System.out.println(obj2.toString());
+        System.out.println(obj3.toString());
     }
 }
--- Agosto8/Pessoa.java
+++ Agosto8/Pessoa.java
@@ -12,8 +12,36 @@ public class Pessoa{
     }
 
     public Pessoa(String nome, int idade, long cpf) {
+        setNome(nome);
+        setIdade(idade);
+        setCpf(cpf);
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
         this.idade = idade;
+    }
+
+    public long getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(long cpf) {
         this.cpf = cpf;
+    }
+
+    public String toString(){
+        return "Nome: " + getNome() + "\nIdade: " + getIdade() + "\nCPF: " + getCpf();
     }
 }
