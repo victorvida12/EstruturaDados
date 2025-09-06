117961744d17453f69efba4b6e481b0dcf5418c1
Victor Vida <168488457+victorvida12@users.noreply.github.com>
2025-09-06 01:19:53 +0000

    Pilha

---
+++ Revisao/Principal.java
@@ -0,0 +1,14 @@
+public class Principal {
+    public static void main(String[] args) {
+        Pilha<String> pilha = new Pilha<String>("Letras");
+
+        pilha.push("A");
+        pilha.push("B");
+        pilha.push("C");
+
+        System.out.println("Dado " + pilha.pop() + " removido da pilha.");
+
+        pilha.imprimePilha();
+
+    }
+}
